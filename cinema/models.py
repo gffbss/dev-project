0be381454921c1dbd0c10652587236b67ff72a4a
Geoff Boss <gboss4@gmail.com>
2014-03-11 17:44:38 -0700

    changed models

--- cinema/models.py
+++ cinema/models.py
@@ -1,19 +1,24 @@
-# from django.db import models
-# from registration.models import User
-#
-#
-# class Movie(models.Model):
-#     year = models.PositiveSmallIntegerField()
-#     length = models.PositiveSmallIntegerField()
-#     title = models.CharField(max_length=275)
-#     genre = models.ForeignKey(Genre)
-#
-#
-# class Genre(models.Model):
-#     genre_type = models.CharField(max_length=255)
-#
-#
-# class Rating(models.Model):
-#     movie = models.ForeignKey(Movie)
-#     user = models.ForeignKey(User)
-#     match = models.BooleanField(default=False)
+from django.db import models
+from registration.models import User
+
+
+class Movie(models.Model):
+    year = models.PositiveSmallIntegerField()
+    length = models.PositiveSmallIntegerField()
+    title = models.CharField(max_length=275)
+    genre = models.ForeignKey(Genre)
+
+
+class Genre(models.Model):
+    genre_type = models.CharField(max_length=255)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie)
+    user = models.ForeignKey(User)
+    match = models.BooleanField(default=False)
+
+class NewUser(User):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=75)
+
