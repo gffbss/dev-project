d70c78c78780a600f8b05315094f414101c9cc3c
Geoff Boss <gboss4@gmail.com>
2014-03-11 17:55:35 -0700

    one more tweak to models

--- cinema/models.py
+++ cinema/models.py
@@ -1,24 +1,28 @@
-from django.db import models
-from registration.models import User
-
-
-class Movie(models.Model):
-    year = models.PositiveSmallIntegerField()
-    length = models.PositiveSmallIntegerField()
-    title = models.CharField(max_length=275)
-    genre = models.ForeignKey(Genre)
-
-
-class Genre(models.Model):
-    genre_type = models.CharField(max_length=255)
-
-
-class Rating(models.Model):
-    movie = models.ForeignKey(Movie)
-    user = models.ForeignKey(User)
-    match = models.BooleanField(default=False)
-
-class NewUser(User):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=75)
-
+# from django.db import models
+# from registration.models import User
+# from django import forms
+# from registration.forms import RegistrationForm
+#
+# class Movie(models.Model):
+#     year = models.PositiveSmallIntegerField()
+#     length = models.PositiveSmallIntegerField()
+#     title = models.CharField(max_length=275)
+#     genre = models.ForeignKey(Genre)
+#
+#
+# class Genre(models.Model):
+#     genre_type = models.CharField(max_length=255)
+#
+#
+# class Rating(models.Model):
+#     movie = models.ForeignKey(Movie)
+#     user = models.ForeignKey(User)
+#     match = models.BooleanField(default=False)
+#
+# class NewUser(User):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=75)
+#
+# class UniqueForm(RegistrationForm):   
+#     first_name = forms.CharField(widget=forms.TextInput, label="first_name")
+#     last_name = forms.CharField(widget=forms.TextInput, label="first_name")
