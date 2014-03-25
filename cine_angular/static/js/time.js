f179cb8f38b96778dfb565accb8d98f154a2ef8a
Geoff Boss <gboss4@gmail.com>
2014-03-25 10:08:20 -0700

    time greetings

--- cine_angular/static/js/time.js
+++ cine_angular/static/js/time.js
@@ -4,12 +4,14 @@
 
 function getTime() {
     ourDate = new Date();
+    goodMorning = 1,2,3,4,5,6,7,8,9,10,11;
 
+    
     console.log(ourDate);
     if (ourDate >= 0 && ourDate < 12){
         console.log('Good Morning');
     }
-    else if (ourDate >= 12 || ourDate <= 17 ){
+    else if (ourDate >= 12 || ourDate < 17 ){
         console.log('Good Afternoon');
     }
     else if (ourDate > 17) {
